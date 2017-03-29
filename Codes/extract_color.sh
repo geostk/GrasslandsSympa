ba676dc42196b14958ca15743acbdbd99d4c2998
Mathieu Fauvel <mfauvel@fauvel-laptop>
2017-03-29 21:20:35 +0200

    Change tangle for extact_colo

--- Codes/extract_color.sh
+++ Codes/extract_color.sh
@@ -1,11 +1,11 @@
-from osgeo import ogr,gdalconst
+for i in `seq 1 192`
+do
+    # Extract the polygon
+    ogr2ogr -f "ESRI Shapefile" /tmp/temp.shp -where "ID = ${i}" all_prairies_indices_emp_auch.shp
 
-# Data name
-inVector = "Data/all_prairies_indices_emp_auch.shp"
-driver = ogr.GetDriverByName('ESRI Shapefile')
-
-# Open data
-vectorIn = driver.Open(inVector, gdalconst.GA_ReadOnly)
-layerIn = vectorIn.GetLayer()
-
-print layerIn.GetFeatureCount()
+    # Warp the aperçu
+    gdalwarp -cutline /tmp/temp.shp -crop_to_cutline -dstalpha /mnt/Data_1/MUESLI/VNIR_SWIR_2m/L1c/apercu/ENVI/MUESLI_mos_VNIR_SWIR_268_208_123_ENVI Figures/color_${i}.tif
+    
+    # Clean data
+    rm /tmp/temp.*
+done
